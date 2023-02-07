80b83b8fd787bf593705dcfcc9adb26b2830c552
Andrew Kirov <AndrewKirov@me.com>
2023-02-07 23:15:53 +0500

    Create migrations for shop app

--- shop/models.py
+++ shop/models.py
@@ -12,4 +12,5 @@ class Course(models.Model):
     price = models.FloatField()
     students_qty = models.IntegerField()
     reviews_qty = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created_at = models.DateTimeField(default=timezone.now)
