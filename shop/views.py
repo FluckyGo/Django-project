19001f0acb05967b821a200fa707f03f615daa6f
Andrew Kirov <AndrewKirov@me.com>
2023-02-09 23:47:07 +0500

    Modify courses template and shop views

--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Course
 
-# Create your views here.
 
-
-def index(reqest):
-    return HttpResponse("Hello from the shop app")
+def index(request):
+    courses = Course.objects.all()
+    return render(request, 'courses.html',  {'courses': courses})
