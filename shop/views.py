40c14e865de420241fe83224637b3fb2347a9793
Andrew Kirov <AndrewKirov@me.com>
2023-02-12 21:06:51 +0500

    Create routing for course page

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 from .models import Course
 
@@ -9,8 +9,5 @@ def index(request):
 
 
 def single_course(request, course_id):
-    try:
-        course = Course.objects.get(pk=course_id)
-        return render(request, 'single_course.html', {'course': course})
-    except Course.DoesNotExist:
-        raise Http404()
+    course = get_object_or_404(Course, pk=course_id)
+    return render(request, 'single_course.html', {'course': course})
